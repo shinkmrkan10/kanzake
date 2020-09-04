4d9ba6ddb782734acb9580898a49db3001259f1d
shinkmrkan10 <shinkmrkan10@gmail.com>
2020-09-04 13:39:56 +0900

    add /

--- index.php
+++ index.php
@@ -6,24 +6,29 @@ html_header($html_title);
 echo <<<EOD
 <body bgcolor="#5f0000" text="#00fffff" link="#00ff00" alink="#0000ff" vlink="#cfcfcf">
 <center>
-<hr width=80%>
+<hr width=80% />
 <a href="kanzake.htm"><img src="jpeg/kanzake.jpg" border=0></a>
-<hr width=80%>
+<hr width=80% />
 <table cellpadding=10>
 <tr><th>
 <a href="kan/"><img src="gif/kan.gif" alt="燗" border=0></a>
-</th><th>
+</th>
+<th>
 <img src="gif/sake.gif" alt="酒" border=0>
-</th><th>
+</th>
+<th>
 <a href="utsuwa/"><img src="gif/utsuwa.gif" alt="器" border=0></a>
-</th><th>
+</th>
+<th>
 <a href="enkai/"><img src="gif/utage.gif" alt="宴" border=0></a>
-</th><th>
+</th>
+<th>
 <img src="gif/siru.gif" alt="知" border=0>
 </th></tr>
 <tr><th>
 <img src="gif/mise.gif" alt="店" border=0>
-</th><th colspan=3>
+</th>
+<th colspan=3>
 EOD;
 
 error_reporting(0);
@@ -73,25 +78,25 @@ else
 {
     $image='yuki-gaeru.gif';
 }
-echo ('<img src="');
-echo ("gif/$image");
-echo ('" border=0><br>');
 echo <<<EOD
+<img src="gif/$image" border=0>
+<br />
 Copyright(c)1998-2000 Chie Ikeda, Shin Kimura
-</th><th>
+</th>
+<th>
 <img src="gif/hito.gif" alt="人" border=0>
 </th></tr>
 </table>
-<hr width=80%>
+<hr width=80% />
 <p>
 <a href="kanzake.htm">ごあいさつ</a>
 　｜　<a href="https://shinkmrkan10.blogspot.jp">燗酒普及協会ブログ</a>
 </p>
-次の活動予定　：現在の情勢を考慮し活動自粛中です。<br>
-2019年10月6日(日)<a href="http://www.taruichi.co.jp/blog/2016/07/post-1594.html">樽一会</a>にて燗付けいたしました。<br>
-2019年6月30日(日)<a href="jpeg/nf2018.jpg">日本酒フェスティバル</a>、協力参加いたしました。<br>
-<hr width=80%>
-　お酒は２０歳になってから．お酒はおいしく適量を．<br>
+次の活動予定　：現在の情勢を考慮し活動自粛中です。<br />
+2019年10月6日(日)<a href="http://www.taruichi.co.jp/blog/2016/07/post-1594.html">樽一会</a>にて燗付けいたしました。<br />
+2019年6月30日(日)<a href="jpeg/nf2018.jpg">日本酒フェスティバル</a>、協力参加いたしました。<br />
+<hr width=80% />
+　お酒は２０歳になってから．お酒はおいしく適量を．<br />
 Copyright(C) 1998-2004 Kanzake Fukyu Kyoukai, All Rights Reserved.
 <ADDRESS>お問い合わせ：燗酒普及協会事務局 (<A HREF="mailto:info@kanzake.org">info@kanzake.org</A>)</ADDRESS>
 </center>
