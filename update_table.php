ecd8e05e2edb1af2ab34b65e5af1d379a2262d44
shinkmrkan10 <shinkmrkan10@gmail.com>
2020-09-05 09:50:03 +0900

    update_table.php

---
+++ update_table.php
@@ -0,0 +1,19 @@
+﻿<?php
+include ('db_login.php');
+require_once ('DB.php');
+    $connection = DB::connect("mysql://$db_username:$db_password@$db_host/$db_database");
+if  ( !$connection )
+{
+    die ("Could not connect to the database :  <br>" . DB::errorMessage( ));
+}
+$query = 'update TABLE counter SET 
+                                a_date = "2020-09-03" 
+                                where accessed LIKE "2020-09-03%"';
+$result = $connection->query($query);
+if (DB::isError($result))
+{
+    die ("Could not query the database : <br>".$query. " ".DB::errorMessage($result));
+}
+echo ("Table created successfully!");
+$connection->disconnect( );
+?>
